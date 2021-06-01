baa8b0b412e0422cfa46bdf3eef4cd6b2a6ed653
mark l chaves <mark@caughtmyeye.cc>
2021-06-02 06:54:19 +0800

    Update my-custom-popup-scripts.php
    Started refactoring.

--- my-custom-popup-scripts.php
+++ my-custom-popup-scripts.php
@@ -12,9 +12,14 @@ function my_custom_popup_scripts() { ?>
         (function ($, document, undefined) {
 	
 			// Launch brochure download form that requires agreed terms first.
-			$( '.launch-brochure-form' ).click(function () {
+			//$( '.launch-brochure-form' ).click(function () {
+			$( '#btn-launch-brochure-download-1' ).click(function () {
 			
 				console.log('[PUM] Clicked on brochure download button.');
+				
+				var p1 = $( '#btn-launch-brochure-download-1' ).data( 'popup1');
+				var p2 = $( '#btn-launch-brochure-download-1' ).data( 'popup2');
+				console.log(`[PUM] Popup 1 is ${p1} and popup 2 is ${p2}`);
 
 				// Wrap your custom code with the following logic to check
 				// for the 30-day cookie.
@@ -22,17 +27,17 @@ function my_custom_popup_scripts() { ?>
 					console.log('[PUM] Already agreed to terms (30 days).');
 					console.log('[PUM] Launching second popup.');
 					// Here, add your custom code to show your second popup.
-					PUM.open(132);
+					PUM.open(parseInt(p2));
 				} else {
-            		// Here, add your custom code to show the terms (first) popup.
+					// Here, add your custom code to show the terms (first) popup.
 					console.log('[PUM] Launching first popup.');
-					PUM.open(190);
+					PUM.open(parseInt(p1));
 				}
-			
+		
 			});
-			
-			// It'd be nice to do this, but it's being blocked right now.
-			// It'd be better to change the plugin code or provide a filter.
+		
+			// It'd be nice to display a checked checkbx, but it's being blocked right
+			// now It'd be better to change the plugin code or provide a filter.
 			// TO DO: Check if there's a filter for this already.
 			$( '#pum-190' ).on('pumAfterOpen', function () {
 				if (PUM.getCookie('pum_tc_agreed_to_terms_30')) {
@@ -42,42 +47,35 @@ function my_custom_popup_scripts() { ?>
 			});
 
 			// Here's where we set the 30-day cookie.
-			$( '#pum-190 input[type="checkbox"]' ).on('pumAgreedToTerms', function () {
+			//$( '#pum-190 input[type="checkbox"]' ).on('pumAgreedToTerms', function () {
+			$( '#pum-tc-input-1' ).on('pumAgreedToTerms', function () {
+
+				var parentOfCheckbox = $( '#pum-tc-input-1' ).parents( '.pum-container' );
+				console.log('[PUM] parentOfCheckbox =', parentOfCheckbox);
+
+				var popupId = parentOfCheckbox.attr('id').split('-').pop();
+
+				// DEBUG
+				console.log(`[PUM] popupId = ${popupId}`);
+
 				console.log('[PUM] Agreed to terms. Setting 30-day cookie.');
 				$.pm_cookie(
-                    'pum_tc_agreed_to_terms_30',
-                    {"190": "agreed"},
-                    '30 days',
-                    '/'
-                );
-				// DEBUG
-				//console.log('[PUM] activeElement 1: ', document.activeElement);
-				//console.log('[PUM] Trying to close first popup.');
-				
-				// ISSUE: There's an issue showing a back-to-back popup when
-				// they use the same default theme or display settings. The second
-				// popup won't show. Below is one code workaround.
-				
-				// Not sure if I need this.
-				//PUM.close(190);
-				
+					'pum_tc_agreed_to_terms_30',
+					{[popupId]: "agreed"},
+					'30 days',
+					'/'
+				);
+
 				// Here, add your custom code to show your second popup.
 				console.log('[PUM] Launching second popup.');
-				
-				// WORKAROUND: Need to force the second to display for some reason.
-				//$('#pum-132').css("display", "block");
-				//$('#pum-132 .pum-content.popmake-content').focus();
-				//console.log('[PUM] activeElement 2: ', document.activeElement);
 
 				// Show the second popup (back-to-back).
 				PUM.open(132);
 				// Will only display if the following CSS is in place (back-to-back fix).
 				// https://gist.github.com/marklchaves/ffb949d75d916b64ed44f71d91ce9459
-				
-				console.log('[PUM] activeElement 3: ', document.activeElement);
 			});
-			
-        }(jQuery, document))
+		
+		}(jQuery, document))
 	</script><?php
 }
 add_action( 'wp_footer', 'my_custom_popup_scripts', 500 );
